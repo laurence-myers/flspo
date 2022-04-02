2b4cd187ac7f7d8762df1076e62b69f7e6e7afe6
Laurence Dougal Myers <laurencedougalmyers@gmail.com>
2022-04-03 00:14:34 +1100

    Remove the existing "By Vendor" directories, if they exist.

--- write.cpp
+++ write.cpp
@@ -1,3 +1,4 @@
+#include <nowide/convert.hpp>
 #include <nowide/iostream.hpp>
 
 #include "write.h"
@@ -6,8 +7,18 @@ void writeToPluginDatabase(const std::filesystem::path &pluginDbPath, const Plug
     const std::filesystem::path effectsOutputPath = pluginDbPath / "Effects" / "By Vendor";
     const std::filesystem::path generatorsOutputPath = pluginDbPath / "Generators" / "By Vendor";
 
+    if (std::filesystem::exists(effectsOutputPath)) {
+        nowide::cout << "Removing existing effects directory: " << nowide::narrow(effectsOutputPath.wstring()) << std::endl;
+        remove_all(effectsOutputPath);
+    }
+
+    if (std::filesystem::exists(generatorsOutputPath)) {
+        nowide::cout << "Removing existing generators directory: " << nowide::narrow(generatorsOutputPath.wstring()) << std::endl;
+        remove_all(generatorsOutputPath);
+    }
+
     for (const auto &entry : pluginByVendorMap) {
-        nowide::cout << entry.first << " - " << entry.second.name << std::endl;
+        nowide::cout << "Writing: " << entry.first << " - " << entry.second.name << std::endl;
         const std::filesystem::path vendorOutputPath = (entry.second.pluginType == PluginType::Effect ? effectsOutputPath : generatorsOutputPath) / entry.first;
         create_directories(vendorOutputPath);
 
